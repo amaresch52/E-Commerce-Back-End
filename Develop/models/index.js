7ab490b210eba6f999cefcf0059754fd3a424ae2
Tom Hanks <amaresch@gmail.com>
2021-06-14 19:17:49 -0500

    starting code and index.js added

---
+++ Develop/models/index.js
@@ -0,0 +1,33 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "categoryId",
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "categoryId",
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: "tagId",
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
